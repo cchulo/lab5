af410b50a286fcd491f519bd8aaa43b9a315d35e
Carlos Chulo <cchulo@ucsd.edu>
2018-02-08 01:11:50 -0800

    completed lab5

--- routes/add.js
+++ routes/add.js
@@ -1,5 +1,10 @@
 var data = require("../data.json");
 
-exports.addFriend = function(req, res) {    
-	// Your code goes here
- }
+exports.addFriend = function(req, res) {
+	var name = req.query.name;
+	var description = req.query.description;
+	var imageURL = "http://lorempixel.com/500/500/people";
+
+	data.friends.push({name: name, description: description, imageURL: imageURL});
+	res.render('index', data);
+}
